9483b97656d7bfdf602bad3301ed1ae3efb8d2ba
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-17 10:01:45 +0200

    reset all document and remove css

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  const title = "My React App";
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+        facere quam modi, nam sed magni similique sit. Obcaecati, amet odio.
+      </p>
+    </div>
+  );
+}
