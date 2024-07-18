bc6553ed80a71cd11fdf3df9a031e1db80a1ac30
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-18 10:56:01 +0200

    create one card and export cars.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 export default function App() {
-  const title = "My React App";
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
-        facere quam modi, nam sed magni similique sit. Obcaecati, amet odio.
-      </p>
-    </div>
+    <ul>
+      <li>
+        <img src="" alt="car" width="" />
+        <h2>
+          Volvo&nbsp;<span>XC90,</span>&nbsp; 2019
+        </h2>
+        <span>$50</span>
+        <p>Lviv</p>
+        <p>Ukraine</p>
+        <p>Auto Rentals</p>
+        <p>Premium</p>
+        <p>Suv</p>
+        <p>Volvo</p>
+        <p>9584</p>
+        <p>Premium Sound System</p>
+        <button>Learn more</button>
+      </li>
+    </ul>
   );
 }
