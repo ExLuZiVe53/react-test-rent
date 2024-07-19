e898d880cc6fe29a5f56e93e0c11c0a46cd24fa1
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-19 21:56:01 +0200

    marcup from one card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,22 @@
+import cars from "../cars.json";
 export default function App() {
+  console.log(cars);
   return (
-    <ul>
+    <ul className="cardList">
       <li>
-        <img src="" alt="car" width="" />
-        <h2>
-          Volvo&nbsp;<span>XC90,</span>&nbsp; 2019
+        <img className="cardImg" src={cars[0].img} alt="car" width="" />
+        <h2 className="cardMake">
+          {cars[0].make}&nbsp;<span>{cars[0].model},</span>&nbsp; {cars[0].year}
         </h2>
-        <span>$50</span>
-        <p>Lviv</p>
-        <p>Ukraine</p>
-        <p>Auto Rentals</p>
-        <p>Premium</p>
-        <p>Suv</p>
-        <p>Volvo</p>
-        <p>9584</p>
-        <p>Premium Sound System</p>
+        <span className="cardRentalPrice">{cars[0].rentalPrice}</span>
+        <p className="cardAddress1">{cars[0].address}</p>
+        <p className="cardAddress2">{cars[0].address}</p>
+        <p className="cardRentalCompany">{cars[0].rentalCompany}</p>
+        <p className="cardRentalCompany0">{cars[0].rentalCompany}</p>
+        <p className="cardType">{cars[0].type}</p>
+        <p className="cardMake">{cars[0].make}</p>
+        <p className="cardId">{cars[0].id}</p>
+        <p className="cardAccessories">{cars[0].accessories[2]}</p>
         <button>Learn more</button>
       </li>
     </ul>
