8896a985ed92dd8698701760b8927fc1fc171f54
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-20 14:17:48 +0200

    create marcup from cardCard and substituted the value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import cars from "../cars.json";
 export default function App() {
   console.log(cars);
+  const cardAddressCity = cars[0].address.split(",")[1];
+  const cardAddressCountry = cars[0].address.split(",")[2];
+
+  const cardFunctionalities = cars[0].functionalities[0].split(",")[0];
+  console.log(cardFunctionalities);
   return (
     <ul className="cardList">
       <li>
@@ -9,14 +14,14 @@ export default function App() {
           {cars[0].make}&nbsp;<span>{cars[0].model},</span>&nbsp; {cars[0].year}
         </h2>
         <span className="cardRentalPrice">{cars[0].rentalPrice}</span>
-        <p className="cardAddress1">{cars[0].address}</p>
-        <p className="cardAddress2">{cars[0].address}</p>
+        <p className="cardAddress1">{cardAddressCity}</p>
+        <p className="cardAddress2">{cardAddressCountry}</p>
         <p className="cardRentalCompany">{cars[0].rentalCompany}</p>
-        <p className="cardRentalCompany0">{cars[0].rentalCompany}</p>
+        <p className="cardRentalCompany0">Premium</p>
         <p className="cardType">{cars[0].type}</p>
         <p className="cardMake">{cars[0].make}</p>
         <p className="cardId">{cars[0].id}</p>
-        <p className="cardAccessories">{cars[0].accessories[2]}</p>
+        <p className="cardAccessories">{cardFunctionalities}</p>
         <button>Learn more</button>
       </li>
     </ul>
