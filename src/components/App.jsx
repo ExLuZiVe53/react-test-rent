c0a80867fcdeea42fabc1ae831303481b5be9b30
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-22 13:24:31 +0200

    added grid-style and render collections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,58 @@
 import cars from "../cars.json";
 export default function App() {
-  console.log(cars);
-  const cardAddressCity = cars[0].address.split(",")[1];
-  const cardAddressCountry = cars[0].address.split(",")[2];
+  // const cardAddressCountry = cars[0].address.split(",")[2];
 
-  const cardFunctionalities = cars[0].functionalities[0].split(",")[0];
-  console.log(cardFunctionalities);
+  // const cardFunctionalities = cars[0].functionalities[0].split(",")[0];
+
+  const arrCars = Array.from(cars);
+  // const cardAddressCity = cars.address.split(",")[1];
   return (
-    <ul className="cardList">
-      <li>
-        <img className="cardImg" src={cars[0].img} alt="car" width="" />
-        <h2 className="cardMake">
-          {cars[0].make}&nbsp;<span>{cars[0].model},</span>&nbsp; {cars[0].year}
-        </h2>
-        <span className="cardRentalPrice">{cars[0].rentalPrice}</span>
-        <p className="cardAddress1">{cardAddressCity}</p>
-        <p className="cardAddress2">{cardAddressCountry}</p>
-        <p className="cardRentalCompany">{cars[0].rentalCompany}</p>
-        <p className="cardRentalCompany0">Premium</p>
-        <p className="cardType">{cars[0].type}</p>
-        <p className="cardMake">{cars[0].make}</p>
-        <p className="cardId">{cars[0].id}</p>
-        <p className="cardAccessories">{cardFunctionalities}</p>
-        <button>Learn more</button>
-      </li>
+    <ul
+      className="cardList"
+      style={{
+        margin: 8,
+        padding: "12px 16px",
+        borderRadius: 4,
+        // backgroundColor: "gray",
+        color: "green",
+        listStyle: "none",
+        display: "grid",
+        flexDirection: "column",
+        gridTemplateColumns: "1fr 1fr 1fr",
+        gap: "10px",
+      }}
+    >
+      {arrCars.length &&
+        arrCars.map((item) => {
+          return (
+            <li
+              style={{
+                margin: 10,
+                padding: "15px 18px",
+                border: "1px solid black",
+                borderRadius: "10px",
+                width: "auto",
+              }}
+              key={`${item.id}_${item.make}`}
+            >
+              <img className="cardImg" src={item.img} alt="car" width="100%" />
+              <h2 className="cardMake">
+                {item.make}&nbsp;<span>{item.model},</span>&nbsp;
+                {item.year}
+              </h2>
+              <span className="cardRentalPrice">{item.rentalPrice}</span>
+              <p className="cardAddress1">cardAddressCity</p>
+              <p className="cardAddress2">cardAddressCountry</p>
+              <p className="cardRentalCompany">{item.rentalCompany}</p>
+              <p className="cardRentalCompany0">Premium</p>
+              <p className="cardType">{item.type}</p>
+              <p className="cardMake">{item.make}</p>
+              <p className="cardId">{item.id}</p>
+              <p className="cardAccessories">cardFunctionalities</p>
+              <button>Learn more</button>
+            </li>
+          );
+        })}
     </ul>
   );
 }
