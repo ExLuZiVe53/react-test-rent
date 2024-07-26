ad1081f136e6d80c4dca3eb232774a5f842bc7a1
ExLuZiVe53 <imstudent27@gmail.com>
2024-07-26 21:43:30 +0200

    breaking down into components and adding styles

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -1,5 +1,59 @@
-const CarCard = () => {
-  return <>CarCard</>;
+import { FaRegHeart } from "react-icons/fa";
+import css from "./CarCard.module.css";
+
+const CarCard = (items) => {
+  const {
+    id,
+    year,
+    make,
+    model,
+    type,
+    img,
+    // description,
+    // fuelConsumption,
+    // engineSize,
+    // accessories,
+    // functionalities,
+    rentalPrice,
+    rentalCompany,
+    // address,
+    // onOpenModal,
+    // rentalConditions,
+    // mileage,
+    // toogleFavorite,
+  } = items;
+
+  const cardAddressCountry = items.address.split(",")[2];
+
+  const cardFunctionalities = items.functionalities[0].split(",")[0];
+
+  // const handleClick = () => {};
+  const cardAddressCity = items.address.split(",")[1];
+  return (
+    <li className={css.cardContainer}>
+      <img className={css.cardContainer__img} src={img} alt="car" width="400" />
+
+      <button className="btn-heart">
+        <FaRegHeart className="icon" size={30} />
+      </button>
+
+      <h2 className="cardMake">
+        {make}&nbsp;
+        <span style={{ color: "green" }}>{model},</span>&nbsp;
+        {year}
+      </h2>
+      <span className="cardRentalPrice">{rentalPrice}</span>
+      <p className="cardAddress1">{cardAddressCity}</p>
+      <p className="cardAddress2">{cardAddressCountry}</p>
+      <p className="cardRentalCompany">{rentalCompany}</p>
+      <p className="cardRentalCompany0">Premium</p>
+      <p className="cardType">{type}</p>
+      <p className="cardMake">{make}</p>
+      <p className="cardId">{id}</p>
+      <p className="cardAccessories">{cardFunctionalities}</p>
+      <button>Learn more</button>
+    </li>
+  );
 };
 
 export default CarCard;
--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,5 +1,38 @@
-const CarList = () => {
-  return <>CarList</>;
+import CarCard from "../CarCard/CarCard";
+import css from "./CarList.module.css";
+
+const CarList = ({ items, FaRegHeart }) => {
+  const showCars = Array.isArray(items) && items.length;
+  console.log(showCars);
+  return (
+    <ul className={css.carList}>
+      {showCars &&
+        items.map((item) => {
+          return (
+            <CarCard
+              key={`${item.id}_${item.make}`}
+              id={item.id}
+              year={item.year}
+              make={item.make}
+              model={item.model}
+              type={item.type}
+              img={item.img}
+              description={item.description}
+              fuelConsumption={item.fuelConsumption}
+              engineSize={item.engineSize}
+              accessories={item.accessories}
+              functionalities={item.functionalities}
+              rentalPrice={item.rentalPrice}
+              rentalCompany={item.rentalCompany}
+              address={item.address}
+              rentalConditions={item.rentalConditions}
+              mileage={item.mileage}
+              iconFavorite={FaRegHeart}
+            />
+          );
+        })}
+    </ul>
+  );
 };
 
 export default CarList;
